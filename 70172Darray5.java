28a627a59eac639ffcd9b7068798f3ac6134ecd1
udaybisht124 <bishtuday4668@gmail.com>
2023-11-11 18:18:42 +0530

    modification done

--- 70172Darray5.java
+++ 70172Darray5.java
@@ -1,7 +1,7 @@
  class equalmatrix {
     public static void main(String[] args) {
-        int[][] matrix1 = {{4,5}, {6, 8}};
-        int[][] matrix2 = {{4,5}, {6, 8}};
+        int[][] mat1 = {{4,5}, {6, 8}};
+        int[][] mat2 = {{4,5}, {6, 8}};
         boolean areEqual1And2 = areMatricesEqual(matrix1, matrix2);
         System.out.println("Matrix 1 and Matrix 2 are equal: " + areEqual1And2);
     }
